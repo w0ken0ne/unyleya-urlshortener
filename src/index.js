c2ac52a64ff437ec35667f7e10817b334cab5561
Willian S <wsapcf@gmail.com>
2022-01-17 19:27:42 -0300

    add first route

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//create express api
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.status(200).send({ message: "Welcome to the API" });
+});
