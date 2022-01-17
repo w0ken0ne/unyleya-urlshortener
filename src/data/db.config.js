183cb60673a84735c988853fafdfec727077108a
Willian S <wsapcf@gmail.com>
2022-01-17 20:02:08 -0300

    add mongoose config on data layer

---
+++ src/data/db.config.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const DB_URI =
+  "mongodb+srv://wsadevv:$secad03cluster0.djwok.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+module.exports = connection;
