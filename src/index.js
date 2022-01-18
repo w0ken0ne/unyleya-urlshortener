6029c70d0ba2f3dacd21716b8d800b7a6096d163
Willian S <wsapcf@gmail.com>
2022-01-17 21:18:57 -0300

    add post route to api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const connectDB = require("./data/db.config");
 const app = express();
+const utils = require("./utils/utils");
 //configure app port
 const port = process.env.PORT || 3000;
 connectDB();
@@ -14,4 +15,19 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   return res.status(200).send({ message: "Welcome to the API" });
+});
+
+app.post("/short", async (req, res) => {
+  const { url } = req.body;
+  if (utils.validateUrl(url)) {
+    try {
+      let url = await Url.findOne({ seedUrl: url });
+      if (url) {
+        return res.status(200).send({
+          message: "Url already has been shortened",
+          url: url.shortUrl,
+        });
+      }
+    } catch (e) {}
+  }
 });
